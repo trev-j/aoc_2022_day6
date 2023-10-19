42a1dee31d96e1dea8c24b3c9813ddd7635aaed8
Trevor Hunter <trevor.j.hunter@outlook.com>
2023-10-19 16:04:23 -0600

    Completed part one of day 2 challenge.

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,85 @@ fn main() {
         process::exit(1);
     });
 
-    let calorie_list_contents = fs::read_to_string(config.file_path).unwrap_or_else(|err| {
+    let input_file_contents = fs::read_to_string(config.file_path).unwrap_or_else(|err| {
         eprintln!("Problem reading input file: {err}");
         process::exit(1);
     });
 
-    let mut max_calories: usize = 0;
-    let mut max_three: Vec<usize> = vec![0,0,0];
-    let mut current_calories: usize = 0;
+    let mut total_score: u32 = 0;
+    const ROCK_VAL: u32 = 1;
+    const PAPER_VAL: u32 = 2;
+    const SCISSORS_VAL: u32 = 3;
 
-    for line in calorie_list_contents.lines() {
-        if line == "" {
-            for i in (0..3).rev() {
-                // Shift max values in result array
-                if max_three[i] == 0 || max_three[i] < current_calories {
-                    for j in 0..i {
-                        max_three[j] = max_three[j + 1];
-                    }
-                    max_three[i] = current_calories;
-                    break;
-                }
-            }
-            current_calories = 0;
+    for line in input_file_contents.lines() {
+        let line_symbols = line.split(" ");
+        let symbols_vec: Vec<&str> = line_symbols.collect();
+        let opponent_play: u32 = played_type_value(symbols_vec[0]);
+        let my_play: u32 = played_type_value(symbols_vec[1]);
+        let mut round_outcome: RoundOutcome = RoundOutcome::Loss;
+
+        if(opponent_play == my_play) {
+            round_outcome = RoundOutcome::Tie;
+        } else if (opponent_play == ROCK_VAL && my_play == PAPER_VAL) {
+            round_outcome = RoundOutcome::Win;
+        } else if (opponent_play == PAPER_VAL && my_play == SCISSORS_VAL) {
+            round_outcome = RoundOutcome::Win;
+        } else if (opponent_play == SCISSORS_VAL && my_play == ROCK_VAL) {
+            round_outcome = RoundOutcome::Win;
         } else {
-            current_calories += line.parse::<usize>().unwrap();
+            round_outcome = RoundOutcome::Loss;
         }
+
+        total_score += round_outcome_value(round_outcome) + my_play;
     }
 
-    for calories in max_three {
-        max_calories += calories;
-    }
+    println!("Total score: {}", total_score);
+}
 
-    println!("Max calories held: {}", max_calories);
+enum StrategySymbolKind {
+    A,
+    B,
+    C,
+    X,
+    Y,
+    Z,
+}
+
+enum RoundOutcome {
+    Win,
+    Tie,
+    Loss,
+}
+
+fn symbol_win_value(symbol: StrategySymbolKind) -> u32 {
+    match symbol {
+        StrategySymbolKind::A => 1,
+        StrategySymbolKind::B => 2,
+        StrategySymbolKind::C => 3,
+        StrategySymbolKind::X => 1,
+        StrategySymbolKind::Y => 2,
+        StrategySymbolKind::Z => 3,
+    }
+}
+
+fn round_outcome_value(symbol: RoundOutcome) -> u32 {
+    match symbol {
+        RoundOutcome::Win => 6,
+        RoundOutcome::Tie => 3,
+        RoundOutcome::Loss => 0,
+    }
+}
+
+fn played_type_value(played_type: &str) -> u32 {
+    match played_type {
+        "A" => 1,
+        "B" => 2,
+        "C" => 3,
+        "X" => 1,
+        "Y" => 2,
+        "Z" => 3,
+        &_ => 0,
+    }
 }
 
 struct Config {
